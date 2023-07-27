ab51396cf8e1d747f129c2ffd48f24d638c06ec7
SebaGoni <sebastiangoni@gmail.com>
2023-07-26 22:40:20 -0300

    Primer commit: Agregado proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola!</h1>
+      <ol>
+        <li>Elemento 1</li>
+        <li>Elemento 2</li>
+        <li>Elemento 3</li>
+        <li>Elemento 4</li>
+        <li>Elemento 5</li>
+      </ol>
+      <img src="/vite.svg" alt="" />
+      
+    </>
+  )
+}
+
+export default App
